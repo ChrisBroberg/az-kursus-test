2224017bdae463ebb09daf5248e075c70836dfe9
Broberg <cbr@delegate.dk>
2022-06-20 17:50:25 +0200

    added function

---
+++ AzureKursusfunction/Function1.cs
@@ -0,0 +1,33 @@
+using System;
+using System.IO;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Azure.WebJobs;
+using Microsoft.Azure.WebJobs.Extensions.Http;
+using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.Logging;
+using Newtonsoft.Json;
+
+namespace AzureKursusfunction
+{
+    public static class Function1
+    {
+        [FunctionName("Function1")]
+        public static async Task<IActionResult> Run(
+            [HttpTrigger(AuthorizationLevel.Function, "get", "post", Route = null)] HttpRequest req,
+            ILogger log)
+        {
+            try
+            {
+                //Get File from request and display name and size
+                var formdata = await req.ReadFormAsync();
+                var file = req.Form.Files["file"];
+                return new OkObjectResult(file.FileName + " - " + file.Length.ToString() +" bytes");
+            }
+            catch (Exception ex)
+            {
+                return new BadRequestObjectResult(ex);
+            }
+        }
+    }
+}
